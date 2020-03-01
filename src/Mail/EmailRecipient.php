fa7457491fe669acf3eae746184d046be88dc251
phillmac <phillmac@users.noreply.github.com>
2020-03-01 14:49:13 +1100

    Add EmailRecipient clsss

---
+++ src/Mail/EmailRecipient.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace UserFrosting\Sprinkle\EmailQueue\Mail;
+
+use UserFrosting\Sprinkle\Core\Mail\EmailRecipient as CoreEmailRecipient;
+
+/**
+ * EmailRecipient Class.
+ *
+ * A class representing a recipient for a MailMessage, with associated parameters.
+ *
+ * @author Alex Weissman (https://alexanderweissman.com)
+ */
+class EmailRecipient extends CoreEmailRecipient implements JsonSerializable
+{
+
+    public function jsonSerialize() {
+        return [
+            'email' => $this->email,
+            'name' => $this->name,
+            'params' => $this->params,
+            'cc' => $this->cc,
+            'bcc' => $this->bcc
+        ];
+    }
+
+    public static function fromData ($data) {
+        if (is_string($data)) {
+            $data = json_decode($data, false);
+        }
+
+        $recipient = new EmailRecipient(
+            $data['email'],
+            $data['name'],
+            $data['params']
+        );
+
+        foreach ($data['cc'] as $cc) {
+            $recipient->cc($cc['email'], $cc['name']);
+        }
+
+        foreach ($data['bcc'] as $bcc) {
+            $recipient->bcc($bcc['email'], $bcc['name']);
+        }
+    }
+
+}
